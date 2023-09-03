218b6bc21e9c0cc53f20dfa8ef705297ee558ac2
santhosh847 <santhoshvijayakumar847@gmail.com>
2023-09-03 13:40:23 +0530

    ensured delay<500ms by fetching asynchrounously

--- Number_system/index.js
+++ Number_system/index.js
@@ -6,6 +6,28 @@ app.get("/", (req, res) => {
 	res.status(200).send("Welcome, explore /numbers route");
 });
 
+async function fetchUrl(url, numberSet) {
+	try {
+		let numbers = await fetch(url);
+		const response = await numbers.json();
+		for (const number of response.numbers) {
+			numberSet.add(number);
+		}
+	} catch (e) {
+		//do nothing if fetch fails
+	}
+}
+
+async function fetchEverything(urls, numberSet) {
+	for (const url of urls) {
+		try {
+			const res = fetchUrl(url, numberSet);
+		} catch (e) {
+			//leave if we can't get particular url
+		}
+	}
+}
+
 app.get("/numbers", async (req, res) => {
 	let urls = req.query.url;
 	var numberSet = new Set();
@@ -18,17 +40,16 @@ app.get("/numbers", async (req, res) => {
 		urls = [urls];
 	}
 	try {
-		for (const url of urls) {
-			let numbers = await fetch(url);
-			const response = await numbers.json();
-			for (const number of response.numbers) {
-				numberSet.add(number);
-			}
-		}
-		let numbers = [];
-		numbers = Array.from(numberSet);
-		numbers = numbers.sort((a, b) => a - b);
-		res.send({ numbers });
+		await fetchEverything(urls, numberSet);
+
+		//wait for 480ms after calling fetch asynchrounously
+		//and then return numbers to ensure delay is not more than 500ms
+		setTimeout(() => {
+			let numbers = [];
+			numbers = Array.from(numberSet);
+			numbers = numbers.sort((a, b) => a - b);
+			res.send({ numbers });
+		}, 480);
 	} catch (e) {
 		res.status(500).send("Service unavailable, make sure url returns numbers");
 		console.log(e);
