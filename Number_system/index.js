8d58ab43de06acd75ad208f48c1c8d0d2ccdbd59
santhosh847 <santhoshvijayakumar847@gmail.com>
2023-09-03 12:08:53 +0530

    localhost:3000/numbers route updated, returns number in unsorted order

--- Number_system/index.js
+++ Number_system/index.js
@@ -3,20 +3,29 @@ const app = express();
 
 app.get("/numbers", async (req, res) => {
 	let urls = req.query.url;
-	let numberSet = new Set();
+	var numberSet = new Set();
 
 	//  if only single url is passed
 	//  make it array
 	if (!Array.isArray(urls)) {
 		urls = [urls];
 	}
-	await urls.forEach(async (url) => {
-		await fetch(url).then(async (res) => {
-			(await res.json()).numbers.forEach((number) => numberSet.add(number));
-		});
-	});
-	console.log(numberSet);
-	res.send("got request");
+	try {
+		for (const url of urls) {
+			let numbers = await fetch(url);
+			const response = await numbers.json();
+			for (const number of response.numbers) {
+				numberSet.add(number);
+			}
+		}
+		let numbers = [];
+		numbers = Array.from(numberSet);
+		numbers.sort();
+		console.log(numbers);
+		res.send(numbers);
+	} catch (e) {
+		res.status(500).send("Service unavailable");
+	}
 });
 
 app.listen(3000, () => {
